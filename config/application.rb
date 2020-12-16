ba4c7937195f8dd9adf49d9981817846cfb47f5d
Rodrigo Serradura <rodrigo.serradura@gmail.com>
2020-12-16 01:57:55 -0300

    Enable active_job and action_mailer

--- config/application.rb
+++ config/application.rb
@@ -3,11 +3,11 @@ require_relative 'boot'
 require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
-# require "active_job/railtie"
+require "active_job/railtie"
 require "active_record/railtie"
 # require "active_storage/engine"
 require "action_controller/railtie"
-# require "action_mailer/railtie"
+require "action_mailer/railtie"
 # require "action_mailbox/engine"
 # require "action_text/engine"
 # require "action_view/railtie"
@@ -33,5 +33,7 @@ module MicroTodoApp
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.active_job.queue_adapter = :async
   end
 end

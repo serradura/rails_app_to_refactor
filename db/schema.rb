96598316e9ea012f70cc666ba90aad9f0926780d
vinibispo <vini.bispo015@gmail.com>
2022-12-15 08:14:12 -0300

    chore(rails): update rails to version 7.0

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_03_035409) do
+ActiveRecord::Schema[6.1].define(version: 2019_12_03_035409) do
 
   create_table "todos", force: :cascade do |t|
     t.string "title"

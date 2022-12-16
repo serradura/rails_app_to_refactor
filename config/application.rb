5f37eb63551cef070d70f66ea03fd77a21eb5393
vinibispo <vini.bispo015@gmail.com>
2022-12-15 21:07:03 -0300

    fix(rails): add letter_opener as delivery_method to get back the old behavior when you create a user

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module MicroTodoApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
+    config.action_mailer.delivery_method = :letter_opener
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading

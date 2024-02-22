6b8ae4c501cc0784d598a2d8785f31325b1ba113
Chris Howe <chris.howe@hireart.com>
2024-02-22 13:05:12 -0600

    improve custom default scope

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_many :todo_lists, dependent: :destroy, inverse_of: :user
   has_many :todos, through: :todo_lists
 
-  has_one :default_todo_list, ->(user) { user.todo_lists.default }, class_name: 'TodoList'
+  has_one :default_todo_list, -> { default }, class_name: 'TodoList'
 
   validates :name, presence: true
   validates :email, presence: true, format: URI::MailTo::EMAIL_REGEXP, uniqueness: true

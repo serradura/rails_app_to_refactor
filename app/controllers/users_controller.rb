6a109e25d55b1a5988787f4b71d540b65a43afa6
Chris Howe <chris.howe@hireart.com>
2024-02-22 10:30:49 -0600

    add user form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,36 +2,12 @@
 
 class UsersController < ApplicationController
   def create
-    user_params = params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    form = UserForm.new(user_params)
 
-    password = user_params[:password].to_s.strip
-    password_confirmation = user_params[:password_confirmation].to_s.strip
-
-    errors = {}
-    errors[:password] = ["can't be blank"] if password.blank?
-    errors[:password_confirmation] = ["can't be blank"] if password_confirmation.blank?
-
-    if errors.present?
-      render_json(422, user: errors)
+    if form.save
+      render_json(201, user: form.user.as_json(only: [:id, :name, :token]))
     else
-      if password != password_confirmation
-        render_json(422, user: { password_confirmation: ["doesn't match password"] })
-      else
-        password_digest = Digest::SHA256.hexdigest(password)
-
-        user = User.new(
-          name: user_params[:name],
-          email: user_params[:email],
-          token: SecureRandom.uuid,
-          password_digest: password_digest
-        )
-
-        if user.save
-          render_json(201, user: user.as_json(only: [:id, :name, :token]))
-        else
-          render_json(422, user: user.errors.as_json)
-        end
-      end
+      render_json(422, user: form.errors.as_json)
     end
   end
 
@@ -46,6 +22,10 @@ class UsersController < ApplicationController
   end
 
   private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 
     def perform_if_authenticated(&block)
       authenticate_user do
---
+++ app/forms/user_form.rb
@@ -0,0 +1,42 @@
+class UserForm
+    include ActiveModel::Model
+
+    attr_accessor :name, :email, :password, :password_confirmation
+
+    validates :password, presence: true
+    validates :password_confirmation, presence: true
+    validate :password_confirmation_matches, if: -> { password.present? || password_confirmation.present? }   
+
+
+    def save
+        return false unless valid?
+
+        password_digest = Digest::SHA256.hexdigest(password)
+        user = User.new(
+          name: name,
+          email: email,
+          token: SecureRandom.uuid,
+          password_digest: password_digest
+        )
+    
+        if user.save
+          @user = user
+          true
+        else
+          errors.merge!(user.errors)
+          false
+        end
+    end
+
+    def user
+        @user
+    end
+
+    private
+    
+    def password_confirmation_matches
+        def password_confirmation_matches
+            errors.add(:password_confirmation, "doesn't match password") if password != password_confirmation
+        end
+    end
+end

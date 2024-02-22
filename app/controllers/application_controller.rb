c3607b0d9c8d8b4450dfed0f8313e04eac77e9df
Chris Howe <chris.howe@hireart.com>
2024-02-22 10:04:45 -0600

    application controller concerns

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,13 @@ class ApplicationController < ActionController::API
   include ActionController::HttpAuthentication::Token::ControllerMethods
   include Authenticatable
   include TodoListable
-  include Errorable
   include Renderable
 
   rescue_from ActionController::ParameterMissing, with: :show_parameter_missing_error
+
+  protected
+    
+  def show_parameter_missing_error(exception)
+      render_json(400, error: exception.message)
+  end
 end
--- app/controllers/concerns/errorable.rb
+++
@@ -1,7 +0,0 @@
-module Errorable
-    protected
-    
-    def show_parameter_missing_error(exception)
-        render_json(400, error: exception.message)
-    end
-end

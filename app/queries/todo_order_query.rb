b789f0e88a7232e9f985757d2e50c10b871bf228
Chris Howe <chris.howe@hireart.com>
2024-02-22 16:30:44 -0600

    dry up order query

--- app/queries/todo_order_query.rb
+++ app/queries/todo_order_query.rb
@@ -1,30 +1,4 @@
-class TodoOrderQuery
+class TodoOrderQuery < BaseOrderQuery
     # Available columns to order by to prevent SQL injection
     ALLOWED_SORT_COLUMNS = ['due_at', 'created_at', 'updated_at', 'title', 'completed_at', 'todo_list_id'].freeze
-    DEFAULT_SORT_COLUMN = 'id'.freeze
-    DEFAULT_SORT_ORDER = :desc.freeze
-  
-    def initialize(relation, params = {})
-      @relation = relation
-      @params = params
-    end
-  
-    def call
-      column = sort_column
-      order = sort_order
-  
-      @relation.order(column => order)
-    end
-  
-    private
-  
-    def sort_column
-      column = @params[:sort_by]&.strip&.downcase
-      ALLOWED_SORT_COLUMNS.include?(column) ? column : DEFAULT_SORT_COLUMN
-    end
-  
-    def sort_order
-      @params[:order]&.strip&.downcase == 'asc' ? :asc : DEFAULT_SORT_ORDER
-    end
-  end
-  
+end
